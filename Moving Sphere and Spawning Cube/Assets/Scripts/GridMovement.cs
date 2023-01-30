155182a0545906ab171a386273360e3ecb5d0f06
Natalia DaLomba <nataliadalomba@protonmail.com>
2023-01-29 19:12:35 -0500

    WIP - Started trying to implement diagonals

--- Moving Sphere and Spawning Cube/Assets/Scripts/GridMovement.cs
+++ Moving Sphere and Spawning Cube/Assets/Scripts/GridMovement.cs
@@ -7,15 +7,46 @@ public class GridMovement : MonoBehaviour
  
     private bool isMoving;
 
+    private bool up;
+    private bool left;
+    private bool back;
+    private bool right;
+
+    private float timeLastPressedUp     = 0;
+    private float timeLastPressedLeft   = 0;
+    private float timeLastPressedBack   = 0;
+    private float timeLastPressedRight  = 0;
+
     //time it takes for player to move from originalPosition to targetPosition. In seconds (so 1/5th of a second)
     private float timeToMove = 0.2f;
+
+    //NOTE: Human reaction time is around 0.13sec (?)
+    //NOTE: 1sec divided into 60 equal parts.. is 0.0167sec
+    //So we NEED more than just THIS frame and the PREVIOUS frame's information.
     
     void Update() {
+        //NOTE: Here, you can grab the PREVIOUS values of up, left, back, and right!
+        up = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        left = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.RightArrow);
+        back = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow);
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) StartMove(Vector3.forward);
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) StartMove(Vector3.left);
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) StartMove(Vector3.back);
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) StartMove(Vector3.right);
+        float time = Time.time;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            timeLastPressedUp = time;
+
+        if (up) StartMove(Vector3.forward);
+        if (left) StartMove(Vector3.left);
+        if (back) StartMove(Vector3.back);
+        if (right) StartMove(Vector3.right);
+
+        //Ex: Up and right..
+        //We want diagonals to happen if we pressed up and right within... 0.4sec (?) ..
+        //IF NOT.. if 0.4sec have passed, move in the direction of the 1st key press
+        float threshold = 0.4f;
+        if (timeLastPressedUp - time >= threshold && timeLastPressedRight - time >= threshold) {
+
+        }
     }
 
     private bool StartMove(Vector3 direction) {
